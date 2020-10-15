9d5d210bccc76e88eed1627bc2bd76db7d00aa19
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-10-15 02:07:14 -0400

    Added refresh tokens

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
     before_action :authorized, only: [:auto_login]
   
     # REGISTER
+    # IN - username, password
+    # OUT - user, accessToken, refreshToken
     def create
       existing_user = User.find_by(username: params[:username])
       # Check if user already exists, if they do return error
@@ -12,7 +14,8 @@ class UsersController < ApplicationController
         @user = User.create(user_params)
         if @user.valid?
           new_access_token = create_access_token(@user.id)
-          render json: {user: @user, token: {accessToken: new_access_token.jwt, accessTokenExpiration: new_access_token.expiration }}
+          new_refresh_token = create_refresh_token(@user.id)
+          render json: {user: @user, auth: {accessToken: new_access_token.jwt, accessTokenExpiration: new_access_token.expiration, refreshToken: new_refresh_token.jwt, refreshTokenExpiration: new_refresh_token.expiration  }}
         else
           render json: {error: "Invalid username or password"}
         end
@@ -21,35 +24,34 @@ class UsersController < ApplicationController
     end
   
     # LOGGING IN
+    # IN - username, password
+    # OUT - user, accessToken, refreshToken
     def login
       @user = User.find_by(username: params[:username])
       # Make sure user exists and password is correct
       if @user && @user.authenticate(params[:password])
-        # Delete previous accessToken now that the user is logging in
-        old_access_token = Blacklist.find_by(jwt:params[:token])
-        # Check if the user has a previous one first
-        if old_access_token
-          Blacklist.find_by(jwt:params[:token]).delete
-        end
-        # now that old accessToken is invalidated make a new one ans 
         new_access_token = create_access_token(@user.id)
-        render json: {user: @user, token: {accessToken: new_access_token.jwt, expiration: new_access_token.expiration }}
-      else
+        new_refresh_token = create_refresh_token(@user.id)
+        render json: {user: @user, auth: {accessToken: new_access_token.jwt, accessTokenExpiration: new_access_token.expiration, refreshToken: new_refresh_token.jwt, refreshTokenExpiration: new_refresh_token.expiration  }}
+      else # User didn't exist or password was incorrect
         render json: {error: "Invalid username or password"}
       end
     end
 
     # REFRESH
+    # IN - refreshToken
+    # OUT - status, accessToken, refreshToken
     def refresh 
-      decoded_token = JWT.decode(params[:token], 's3cr3t', true, algorithm: 'HS256')
+      decoded_refresh_token = JWT.decode(params[:refreshToken], 's3cr3t', true, algorithm: 'HS256')
       # Check if token was decoded
-      if decoded_token
-          @user = User.find_by(id: decoded_token[0]['user_id'])
+      if decoded_refresh_token
+          @user = User.find_by(id: decoded_refresh_token[0]['user_id'])
           if @user # user exists
-              Blacklist.find_by(jwt: params[:token]).delete
+              Blacklist.find_by(jwt: params[:refreshToken]).delete
               # update the device_key for the user
               new_access_token = create_access_token(@user.id)
-              render json: {status: "Refreshed Access Token", token: {accessToken: new_access_token.jwt, expiration: new_access_token.expiration }}
+              new_refresh_token = create_refresh_token(@user.id)
+              render json: {status: "Refreshed Tokens", auth: {accessToken: new_access_token.jwt, accessTokenExpiration: new_access_token.expiration, refreshToken: new_refresh_token.jwt, refreshTokenExpiration: new_refresh_token.expiration  }}
           else
               render json: {error: "Invalid User"}
           end
@@ -59,14 +61,16 @@ class UsersController < ApplicationController
     end
 
     # Update Device Key
+    # IN - deviceKey, accessToken
+    # OUT - status
     def device_key
-        decoded_token = JWT.decode(params[:token], 's3cr3t', true, algorithm: 'HS256')
-        # Check if token was decoded
-        if decoded_token
-            @user = User.find_by(id: decoded_token[0]['user_id'])
+        decoded_access_token = JWT.decode(params[:accessToken], 's3cr3t', true, algorithm: 'HS256')
+
+        if decoded_access_token # Check if token was decoded
+            @user = User.find_by(id: decoded_access_token[0]['user_id'])
             if @user # user exists
                 # Make usre accessToken is valid
-                access_token_record = Blacklist.find_by(jwt: params[:token])
+                access_token_record = Blacklist.find_by(jwt: params[:accessToken])
                 if access_token_record && Time.now < access_token_record.expiration 
                   # update the device_key for the user
                   @user.update(device_key: params[:deviceKey])
@@ -74,8 +78,7 @@ class UsersController < ApplicationController
                 else
                   render json: {status: "Token Expired"}
                 end
-                
-            else
+            else # User does not exist
                 render json: {error: "Invalid User"}
             end
         else # token is null
@@ -84,15 +87,17 @@ class UsersController < ApplicationController
     end
 
     # LOGOUT
+    # IN - accessToken
+    # OUT - status
     def logout
-      decoded_token = JWT.decode(params[:token], 's3cr3t', true, algorithm: 'HS256')
+      decoded_access_token = JWT.decode(params[:accessToken], 's3cr3t', true, algorithm: 'HS256')
         # Check if token was decoded
-        if decoded_token
-            @user = User.find_by(id: decoded_token[0]['user_id'])
+        if decoded_access_token
+            @user = User.find_by(id: decoded_access_token[0]['user_id'])
             if @user # user exists
                 # Invalidate their access token
                 # Next time they try to access resources with their access token they will be denied
-                Blacklist.find_by(jwt: params[:token]).delete
+                Blacklist.find_by(jwt: params[:accessToken]).delete
                 render json: {status: "User was succesfully logged out"}
             else
                 render json: {error: "Invalid User"}
@@ -103,22 +108,25 @@ class UsersController < ApplicationController
     end
 
     # DELETE
+    # IN - accessToken, refreshToken
+    # OUT - status
     def delete
-      decoded_token = JWT.decode(params[:token], 's3cr3t', true, algorithm: 'HS256')
-        # Check if token was decoded
-        if decoded_token
-            @user = User.find_by(id: decoded_token[0]['user_id'])
-            if @user # user exists
-                Blacklist.find_by(jwt: params[:token]).delete
-                # update the device_key for the user
-                User.find_by(id: @user.id).delete
-                render json: {status: "User was succesfully deleted"}
-            else
-                render json: {error: "Invalid User"}
-            end
-        else # token is null
-            render json: {error: "Invalid Token"}
-        end
+      decoded_access_token = JWT.decode(params[:accessToken], 's3cr3t', true, algorithm: 'HS256')
+      decoded_refresh_token = JWT.decode(params[:refreshToken], 's3cr3t', true, algorithm: 'HS256')
+      # Check if token was decoded
+      if decoded_access_token && decoded_refresh_token
+          @user = User.find_by(id: decoded_access_token[0]['user_id'])
+          if @user # user exists
+              Blacklist.find_by(jwt: params[:accessToken]).delete
+              Blacklist.find_by(jwt: params[:refreshToken]).delete
+              User.find_by(id: @user.id).delete
+              render json: {status: "User was succesfully deleted"}
+          else
+              render json: {error: "Invalid User"}
+          end
+      else # token is null
+          render json: {error: "Invalid Tokens"}
+      end
     end
   
   
@@ -130,11 +138,19 @@ class UsersController < ApplicationController
       ('0'..'z').to_a.shuffle.first(8).join
     end
 
-    # Creates a new Blacklist token with expiration to Blacklist table and returns it
+    # Creates JWT that expires in 15 mintues and contains user id, token type, and a salt
     def create_access_token(user_id)
       # Create new access token
       token = JWT.encode({user_id: user_id, type: "access", salt: token_salt()}, 's3cr3t')
-      exp_time = Time.now + 15*60
+      exp_time = Time.now + 15*60 # 15 minutes
+      Blacklist.create({ jwt: token, expiration: exp_time })
+    end
+
+    # Creates JWT that expires in 30 days and contains user id, token type, and a salt
+    def create_refresh_token(user_id)
+      # Create new access token
+      token = JWT.encode({user_id: user_id, type: "refresh", salt: token_salt()}, 's3cr3t')
+      exp_time = Time.now + 30*24*60*60 # 30 days
       Blacklist.create({ jwt: token, expiration: exp_time })
     end
   
